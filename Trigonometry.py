28fdfc69bf43a2c81b90149fe93129158902a2ea
Santiago Sierra <santiagosierravasquez@gmail.com>
2022-02-26 20:55:24 -0500

    primer commit

---
+++ Trigonometry.py
@@ -0,0 +1,23 @@
+from cmath import sin
+
+
+def main():
+    opcions = {
+        1 : "area",
+        2 : "perimetro"
+    }
+    opcion = input("""
+        1 = area
+        2 = perimetro
+        """)
+
+    if opcion == "area":
+        pass
+    
+    if opcion == "perimetro":
+        pass
+
+
+
+if __name__ == "__main__":
+    main()
